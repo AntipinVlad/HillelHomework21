c198abe1eafa1e8e71243cc7f7829672bec0dfba
AntipinVlad <antipinmail1@gmail.com>
2024-02-12 21:55:20 +0200

    fix code regarding to comments

--- src/Helpers/DrinksMachine.java
+++ src/Helpers/DrinksMachine.java
@@ -1,10 +1,20 @@
 package Helpers;
 
 public enum DrinksMachine {
-    COFFEE,
-    TEA,
-    LEMONADE,
-    MOJITO,
-    MINERAL_WATER,
-    COCA_COLA;
+    COFFEE("COFFEE"),
+    TEA("TEA"),
+    LEMONADE("LEMONADE"),
+    MOJITO("MOJITO"),
+    MINERAL_WATER("MINERAL_WATER"),
+    COCA_COLA("COCA_COLA");
+
+    private String drinkName;
+
+    DrinksMachine(String name) {
+        this.drinkName = name;
+    }
+
+    public String getDrinkName() {
+        return drinkName;
+    }
 }
--- src/Homework21Program.java
+++ src/Homework21Program.java
@@ -1,40 +1,50 @@
 import Helpers.DrinksMachine;
 import Modules.Drinks;
 
-import java.util.Arrays;
 import java.util.Scanner;
+
+import static Helpers.DrinksMachine.*;
+import static Modules.Drinks.*;
 
 public class Homework21Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.printf("Select a drink by writing a number from 0 to 5: %s \nFor Exit write 6 \n",
-                    Arrays.toString(DrinksMachine.values()));
+            System.out.printf("Select a drink by writing a number from 0 to 5: \n"
+                            + "0 %s: %,.2f \n"
+                            + "1 %s: %,.2f \n"
+                            + "2 %s: %,.2f \n"
+                            + "3 %s: %,.2f \n"
+                            + "4 %s: %,.2f \n"
+                            + "5 %s: %,.2f \n"
+                            + "6 Exit \n",
+                    COFFEE, COFFEE_PRICE, TEA, TEA_PRICE, LEMONADE, LEMONADE_PRICE, MOJITO, MOCHITO_PRICE, MINERAL_WATER, MINERAL_WATER_PRICE, COCA_COLA, COCA_COLA_PRICE);
 
-            int choice = scanner.nextInt();
+            String choice = scanner.nextLine();
             DrinksMachine drink;
 
             switch (choice) {
-                case 0:
-                    drink = DrinksMachine.COFFEE;
+                case "0":
+                    drink = COFFEE;
                     break;
-                case 1:
+                case "1":
                     drink = DrinksMachine.TEA;
                     break;
-                case 2:
+                case "2":
                     drink = DrinksMachine.LEMONADE;
                     break;
-                case 3:
+                case "3":
                     drink = DrinksMachine.MOJITO;
                     break;
-                case 4:
+                case "4":
                     drink = DrinksMachine.MINERAL_WATER;
                     break;
-                case 5:
+                case "5":
                     drink = DrinksMachine.COCA_COLA;
                     break;
-                case 6:
+                case "6":
                     Drinks.displayTotal();
+                    scanner.close();
                     return;
                 default:
                     System.out.println("Invalid choice.");
